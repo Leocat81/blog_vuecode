543abc0558f6bd82ecc7e531733f0453d694bd61
pipe <15283666096@163.com>
2021-08-03 10:58:17 +0800

    add:滑动穿透

--- docs/.vuepress/components/ScrollThrough.vue
+++ docs/.vuepress/components/ScrollThrough.vue
@@ -4,12 +4,14 @@
     <button type="button" @click="open">打开弹窗</button>
     <div v-if="show" class="dialog__wrapper">
       <div class="dialog">
-        <p class="close_dialog" @click="show=false">✖</p>
-        <ul class="dialog_body">
-          <li v-for="i in 10">
-            {{i}}
-          </li>
-        </ul>
+        <p class="close_dialog" @click="close">❎</p>
+        <div class="dialog_body">
+          <ul>
+            <li v-for="i in 10" @click="close">
+              {{i}}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <div class="popup"></div>
@@ -18,6 +20,7 @@
 
 <script>
 import { Toast } from "zen-toast";
+import BScroll from "better-scroll";
 import Vue from "Vue";
 Vue.use(Toast);
 export default {
@@ -26,12 +29,33 @@ export default {
       show: false,
     };
   },
+  props: ["BetterScroll"],
   //生命周期 - 挂载完成（访问DOM元素）
   mounted() {},
   methods: {
+    close() {
+      this.show = false;
+    },
     open() {
       if (!this.agent()) {
         this.show = true;
+        if (this.BetterScroll !== undefined) {
+          this.$nextTick(() => {
+            new BScroll(".dialog_body", {
+              pullUpLoad: true,
+              scrollbar: true,
+              click: true,
+              pullDownRefresh: true,
+              scrollY: true,
+            });
+          });
+        } else {
+          this.$nextTick(() => {
+            let bodyDom = document.getElementsByClassName("dialog_body")[0];
+            debugger;
+            bodyDom.style.overflow = "scroll";
+          });
+        }
       } else {
         Toast.fail("请在移动端中查看");
       }
@@ -77,6 +101,7 @@ export default {
   overflow: auto;
   margin: 0;
   background-color: rgb(200, 200, 200, 0.8);
+  z-index: 9999;
 }
 .dialog {
   position: relative;
@@ -86,13 +111,13 @@ export default {
   background: #fff;
   box-shadow: 0 1px 3px rgb(0 0 0 / 30%);
   box-sizing: border-box;
-  width: 50%;
-  height: 100px;
+  width: 60%;
+  height: 200px;
 }
 .dialog_body {
   width: 100%;
-  height: 100%;
-  overflow-y: scroll;
+  height: 200px;
+  overflow: hidden;
 }
 .popup {
   width: 200px;
@@ -103,7 +128,9 @@ export default {
   margin: 0;
   line-height: normal;
   position: absolute;
-  right: 0;
+  right: 10px;
   top: 0;
+  text-align: right;
+  width: 100%;
 }
 </style>
--- docs/vueguide/scrollthrough.md
+++ docs/vueguide/scrollthrough.md
@@ -6,5 +6,25 @@
 
 <ScrollThrough/>
 
+## 解决方案
+
+- 采用 BetterScroll npm 包来解决
+
+```js
+/* 第一步：引入better-scroll */
+import BScroll from "better-scroll";
+/* 第二部: 初始化 此处需注意初始的节点必须存在，要注意节点上是否有v-if,否则会提示节点找不到 */
+new BScroll(".dialog_body", {
+  pullUpLoad: true,
+  scrollbar: true,
+  click: true,
+  pullDownRefresh: true,
+  scrollY: true,
+});
+```
+<ScrollThrough BetterScroll/>
+
+<!-- TODO:解决遮罩层依然滚动问题 -->
 ## 参考文献
+
 <a href="https://www.cnblogs.com/padding1015/p/10568070.html" target="_blank">滑动穿透六种解决方案</a>
