40a78d13cadf0cbd80d4f7d55ac2dd8eecc37828
pipe <15283666096@163.com>
2022-02-12 23:48:16 +0800

    add:暂时去掉pixi.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,16 +19,16 @@ module.exports = {
   base: "/pipepandafeng/",
   title: "笔记", // 设置网站标题
   description: "欢迎来到我的博客",
-  head: [
-    ["link", { rel: "icon", href: "/img/panda4.png" }],
-    [
-      "script",
-      {
-        src:
-          "https://cdn.jsdelivr.net/npm/pixi.js@6.1.2/dist/browser/pixi.min.js",
-      },
-    ],
-  ],
+  // head: [
+  //   ["link", { rel: "icon", href: "/img/panda4.png" }],
+  //   [
+  //     "script",
+  //     {
+  //       src:
+  //         "https://cdn.jsdelivr.net/npm/pixi.js@6.1.2/dist/browser/pixi.min.js",
+  //     },
+  //   ],
+  // ],
   markdown: {
     lineNumbers: true,
   },
