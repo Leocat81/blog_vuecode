9fff699131458ea27f6d7e73c5e95dfa30fd2a04
pipe <15283666096@163.com>
2022-12-07 16:31:49 +0800

    add:优化界面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,33 +49,33 @@ module.exports = {
     ["autometa", autometa_options],
     "vuepress-plugin-baidu-autopush",
     /* 两个插件只能选一个 */
-    [
-      "vuepress-plugin-helper-live2d",
-      {
-        // 是否开启控制台日志打印(default: false)
-        log: true,
-        live2d: {
-          // 是否启用(关闭请设置为false)(default: true)
-          enable: true,
-          // 模型名称(default: hibiki)>>>取值请参考：
-          // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
-          model: "hibiki",
-          display: {
-            position: "right", // 显示位置：left/right(default: 'right')
-            width: 135, // 模型的长度(default: 135)
-            height: 300, // 模型的高度(default: 300)
-            hOffset: 65, //  水平偏移(default: 65)
-            vOffset: 0, //  垂直偏移(default: 0)
-          },
-          mobile: {
-            show: false, // 是否在移动设备上显示(default: false)
-          },
-          react: {
-            opacity: 0.8, // 模型透明度(default: 0.8)
-          },
-        },
-      },
-    ],
+    // [
+    //   "vuepress-plugin-helper-live2d",
+    //   {
+    //     // 是否开启控制台日志打印(default: false)
+    //     log: true,
+    //     live2d: {
+    //       // 是否启用(关闭请设置为false)(default: true)
+    //       enable: false,
+    //       // 模型名称(default: hibiki)>>>取值请参考：
+    //       // https://github.com/JoeyBling/hexo-theme-yilia-plus/wiki/live2d%E6%A8%A1%E5%9E%8B%E5%8C%85%E5%B1%95%E7%A4%BA
+    //       model: "hibiki",
+    //       display: {
+    //         position: "left", // 显示位置：left/right(default: 'right')
+    //         width: 15, // 模型的长度(default: 135)
+    //         height: 10, // 模型的高度(default: 300)
+    //         hOffset: 65, //  水平偏移(default: 65)
+    //         vOffset: 0, //  垂直偏移(default: 0)
+    //       },
+    //       mobile: {
+    //         show: false, // 是否在移动设备上显示(default: false)
+    //       },
+    //       react: {
+    //         opacity: 0.8, // 模型透明度(default: 0.8)
+    //       },
+    //     },
+    //   },
+    // ],
     ["vuepress-plugin-gotop-plus"],
   ],
   themeConfig: {
@@ -96,6 +96,11 @@ module.exports = {
       },
     ],
     sidebar: [
+      {
+        title: "序言",
+        collapsable: false,
+        path: 'home/',
+      },
       {
         title: "Funny",
         collapsable: true,
